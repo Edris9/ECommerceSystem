653a0d8956c9b46cf5527912bc46f0d8145fe204
Edris9 <Edriskohestani1010@gmail.com>
2025-09-29 13:36:10 +0200

    Add login functionality for Customer and Admin with hashed passwords

--- Customer.cs
+++ Customer.cs
@@ -1,6 +1,5 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
-
 namespace ECommerceSystem
 {
     internal class Customer
@@ -10,20 +9,36 @@ namespace ECommerceSystem
         public string LastName { get; set; }
         private string Email { get; set; }
         private string Password { get; set; }
+
         // get user details
-        public void CreateAccount()
+        public virtual void CreateAccount()
         {
             Console.WriteLine("Enter your name:");
             Name = Console.ReadLine();
             Console.WriteLine("Enter your last name:");
             LastName = Console.ReadLine();
-
             Console.WriteLine("Enter your email:");
             Email = Console.ReadLine();
             Console.WriteLine("Enter your password:");
-            Password = Console.ReadLine();
+            Password = HashPassword(Console.ReadLine());
             Console.WriteLine($"\nAccount successfully created for {Name} {LastName}!");
         }
 
+        // Hash password
+        protected string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return Convert.ToBase64String(bytes);
+            }
+        }
+
+        // Login metod
+        public bool Login(string email, string password)
+        {
+            string hashedInput = HashPassword(password);
+            return this.Email == email && this.Password == hashedInput;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,37 +2,101 @@
 {
     internal class Program
     {
+        static List<Customer> customers = new List<Customer>();
+        static List<Admin> admins = new List<Admin>();
+        static List<Product> products = new List<Product>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Welcome to ECommerceSystem:\nChoose 1 if you are customer\nChoose 2 if you are admin\nChoose 3 to create an account.");
-            int choice = Convert.ToInt32(Console.ReadLine());
-            if (choice == 1)
-            {
-                // TODO: Customer Login (kommer senare)
-                Console.WriteLine("Customer login - Coming soon!");
-            }
-            else if (choice == 2)
-            {
-                // TODO: Admin Login (kommer senare)
-                Console.WriteLine("Admin login - Coming soon!");
-            }
-            else if (choice == 3)
-            {
-                Console.WriteLine("Create account as: 1) Customer  2) Admin");
-                int accountType = Convert.ToInt32(Console.ReadLine());
+            bool running = true;
 
-                if (accountType == 1)
+            while (running)
+            {
+                Console.WriteLine("\n=== E-Commerce System ===");
+                Console.WriteLine("1. Customer Login");
+                Console.WriteLine("2. Admin Login");
+                Console.WriteLine("3. Create Account");
+                Console.WriteLine("4. Exit");
+                Console.Write("Choose option: ");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 1)
                 {
-                    Customer customer = new Customer();
-                    customer.CreateAccount();
+                    CustomerLogin();
                 }
-                else if (accountType == 2)
+                else if (choice == 2)
                 {
-                    Admin admin = new Admin();
-                    admin.CreateAccount();
+                    AdminLogin();
+                }
+                else if (choice == 3)
+                {
+                    CreateAccount();
+                }
+                else if (choice == 4)
+                {
+                    running = false;
+                    Console.WriteLine("Goodbye!");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option!");
                 }
             }
+        }
 
+        static void CustomerLogin()
+        {
+            Console.WriteLine("\nCustomer Login");
+            Console.Write("Enter email: ");
+            string email = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            foreach (var customer in customers)
+            {
+                if (customer.Login(email, password))
+                {
+                    Console.WriteLine($"Welcome back, {customer.Name}!");
+                    return;
+                }
+            }
+            Console.WriteLine("Invalid email or password.");
+        }
+
+        static void AdminLogin()
+        {
+            Console.WriteLine("\nAdmin Login");
+            Console.Write("Enter email: ");
+            string email = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+            foreach (var admin in admins)
+            {
+                if (admin.Login(email, password))
+                {
+                    Console.WriteLine($"Welcome back, Admin {admin.Name}!");
+                    return;
+                }
+            }
+        }
+        static void CreateAccount()
+        {
+            Console.WriteLine("\nCreate account as: 1) Customer  2) Admin");
+            int accountType = Convert.ToInt32(Console.ReadLine());
+
+            if (accountType == 1)
+            {
+                Customer customer = new Customer();
+                customer.CreateAccount();
+                customers.Add(customer); // Spara i listan!
+            }
+            else if (accountType == 2)
+            {
+                Admin admin = new Admin();
+                admin.CreateAccount();
+                admins.Add(admin); // Spara i listan!
+            }
         }
     }
 }
