f6bd05e69a93e9218b381e7aed749407216fbf71
Edris9 <Edriskohestani1010@gmail.com>
2025-09-29 09:55:37 +0200

    customer class and methods successfully created

--- Program.cs
+++ Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Welcome to ECommerceSystem:\nChoose 1 if you are customer\nChoose 2 if you are admin\nChoose 3 to create an account.");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 1)
+            {
+                Customer customer = new Customer();
+                customer.CreateAccount();
+            }
+            else if (choice == 2)
+            {
+                Admin admin = new Admin();
+            }
+            else if (choice == 3)
+            {
+                CreateAccount createAccount = new CreateAccount();
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please restart the application and choose a valid option.");
+            }
+            
         }
     }
 }
